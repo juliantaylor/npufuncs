3758ef27c3682738e32615ce441867bda81d9033
Julian Taylor <jtaylor.debian@googlemail.com>
2014-01-08 22:35:47 +0100

    add -ffast-math and -ffp-contract=fast to CFLAGS to ensure fma even with gcc-4.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def configuration(parent_package='', top_path=None):
     config = Configuration('.',
                            parent_package,
                            top_path)
-    config.add_extension('npfma', ['fma.c', 'fma-avx.c', 'fma-fma3.c', 'fma-fma4.c', 'fma-sse2.c'], extra_compile_args=['-std=c99', '-O3'])
+    config.add_extension('npfma', ['fma.c', 'fma-avx.c', 'fma-fma3.c', 'fma-fma4.c', 'fma-sse2.c'], extra_compile_args=['-std=c99', '-O3', '-ffast-math', '-ffp-contract=fast'])
 
     return config
 
